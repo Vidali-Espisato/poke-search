f1a0e3d83c76aae8280f9c7a7942aea5d66fd7a5
Manish Roy <manish.11612939@lpu.in>
2023-07-01 00:07:40 +0530

    introduced theme-provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { ThemeProvider } from '@/components/themeProvider'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <ThemeProvider attribute='class' defaultTheme='dark'>
+          <main className="h-screen p-24">
+            <div className='flex items-center'>
+              <Image src="/pokeball.png" alt="pokeball" width={60} height={60} className='mt-4 mr-2 animate-bounce rounded-full shadow-md shadow-yellow-200' />
+              <Image src="/poke.png" alt="pokeball" width={110} height={50} />
+              <h2 className='font-bold text-4xl border-4 border-yellow-300 px-1 bg-blue-600 text-yellow-200 rounded-2xl shadow-md shadow-yellow-200'>Searx</h2>
+            </div>
+            <div className='font-mono'>
+
+            </div>
+            {children}
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
