bda59bede64657cf86bbdb75d5d83f58406db93d
Manish Roy <manish.11612939@lpu.in>
2023-07-01 18:02:37 +0530

    added search box and pokemons grid

---
+++ components/pokemons.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { useState } from "react"
+import PokemonCard from "./pokemonPill"
+import { Input } from "./ui/input"
+
+
+interface pokemonsProps {
+    pokemons: any[]
+}
+
+export default function({ pokemons }: pokemonsProps) {
+    const [searchText, setSearchText] = useState("")
+
+    const filteredPokemons = pokemons?.filter((pokemon: any) => pokemon.name.toLowerCase().includes(searchText.toLowerCase()))
+
+
+    return (
+        <div className="mx-auto w-1/2 h-screen">
+            <Input
+                type="text"
+                value={searchText}
+                id="pokemonName"
+                autoComplete="off"
+                placeholder="Type your Pokémon name..."
+                onChange={e => setSearchText(e.target.value.trim())}
+                className="border-4 border-zinc-500 rounded-xl text-lg text-center py-6 font-semibold placeholder:text-zinc-500 placeholder:italic"
+            />
+            <div className="my-5 grid text-center xl:grid-cols-3 lg:grid-cols-2 gap-x-10 gap-y-5 h-4/5 overflow-y-scroll no-scrollbar">
+                {
+                    filteredPokemons?.map((pokemon: any) => (
+                        <PokemonCard name={ pokemon.name } />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
