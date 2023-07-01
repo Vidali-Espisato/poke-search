d3ede7048532df39806f6e7bd964b02543e3ce5c
Manish Roy <manish.11612939@lpu.in>
2023-07-01 23:00:49 +0530

    added api for getting pokemon data

--- lib/apis.ts
+++ lib/apis.ts
@@ -1,9 +1,17 @@
 
-const API_BASE_URL = "https://pokeapi.co/api/v2/"
+const API_BASE_URL = "https://pokeapi.co/api/v2/pokemon/"
 
 export async function fetchPokemons() {
 
-    const response = await fetch(API_BASE_URL + "pokemon?limit=151&offset=0")
+    const response = await fetch(API_BASE_URL + "?limit=151&offset=0")
     const data = await response.json()
     return data.results
+}
+
+export async function getPokemon(pokemonName: string) {
+
+    const response = await fetch(API_BASE_URL + pokemonName)
+    const data = await response.json()
+    console.log("data", data)
+    return data
 }
