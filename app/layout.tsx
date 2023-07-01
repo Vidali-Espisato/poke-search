f513eddd15869db8904f2251023f0fb005b243c0
Manish Roy <manish.11612939@lpu.in>
2023-07-01 18:00:55 +0530

    updated styling in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,16 +19,17 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeProvider attribute='class' defaultTheme='dark'>
-          <main className="h-screen p-24">
-            <div className='flex items-center'>
-              <Image src="/pokeball.png" alt="pokeball" width={60} height={60} className='mt-4 mr-2 animate-bounce rounded-full shadow-md shadow-yellow-200' />
-              <Image src="/poke.png" alt="pokeball" width={110} height={50} />
-              <h2 className='font-bold text-4xl border-4 border-yellow-300 px-1 bg-blue-600 text-yellow-200 rounded-2xl shadow-md shadow-yellow-200'>Searx</h2>
+          <main className="h-screen overflow-hidden">
+            <div className='flex items-center justify-center my-8'>
+              <Image src="/pokeball.png" alt="pokeball" width={40} height={40} className='mt-4 mr-2 animate-bounce rounded-full shadow-md shadow-yellow-200' />
+              <Image src="/poke.png" alt="poke" width={90} height={40} />
+              <h2
+                className='font-bold text-3xl border-4 border-yellow-300 px-1 bg-blue-600 text-yellow-200 rounded-2xl shadow-md shadow-yellow-200 mr-2'
+              >Searx</h2>
             </div>
             <div className='font-mono'>
-
+              {children}
             </div>
-            {children}
           </main>
         </ThemeProvider>
       </body>
